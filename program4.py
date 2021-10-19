04aa3208bac72b10582a470cd606b27ed250913b
gjorgiev <gjorgiev991@gmail.com>
2021-10-19 16:12:32 +0200

    added .gitignore

---
+++ program4.py
@@ -0,0 +1,6 @@
+class Solution:
+    def findMedianSortedArrays(self, nums1: list[int], nums2: list[int]) -> float:
+        
+
+solution = Solution()
+solution.findMedianSortedArrays([1,2,3], [4,5,6])
