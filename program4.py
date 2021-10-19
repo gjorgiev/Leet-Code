4b398887adce6107761fde39d3ea3bd2ce3495f9
gjorgiev <gjorgiev991@gmail.com>
2021-10-19 16:30:50 +0200

    merge arrays

--- program4.py
+++ program4.py
@@ -1,6 +1,24 @@
 class Solution:
+    def merge_arrays(self, nums1: list[int], nums2: list[int], m, n) -> list[int]:
+        nums3 = [None] * (m + n)
+
+        i = j = k = 0
+
+        # Traverse both arrays
+        while i < m and j < n:
+            if nums[i] < nums[j]:
+                nums3[k] = nums[i]
+                i+=1
+                k+=1
+            else:
+                nums3[k] = nums[j]
+                j+=1
+                k+=1
+                
+
+
     def findMedianSortedArrays(self, nums1: list[int], nums2: list[int]) -> float:
-        
+        pass
 
 solution = Solution()
 solution.findMedianSortedArrays([1,2,3], [4,5,6])
